c06d4620c0e59b6fd4e7555f09ece55937aeb70f
codelinshan <34505246+codelinshan@users.noreply.github.com>
2021-04-21 10:08:39 +0800

    Update dailyFudan.py

--- dailyFudan.py
+++ dailyFudan.py
@@ -143,7 +143,6 @@ class Zlapp(Fudan):
 
         if last_info["d"]["info"]["date"] == today:
             logging.info("今日已提交")
-            self.close()
         else:
             logging.info("未提交")
             self.last_info = last_info["d"]["info"]
